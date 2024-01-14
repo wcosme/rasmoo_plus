cf5aaa6e89677ff2d1fdc7c53b0051b684fb5d62
Wallace Gonçalves <wallacecosme@gmail.com>
2024-01-14 17:31:11 -0300

    Add users type

---
+++ src/main/java/com/client/ws/rasmoo/plus/model/User.java
@@ -0,0 +1,40 @@
+package com.client.ws.rasmoo.plus.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "users_id")
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String cpf;
+
+    @Column(name = "dt_subscription")
+    private LocalDate dtSubscription = LocalDate.now();
+
+    @Column(name = "dt_expiration")
+    private LocalDate dtExpiration;
+
+    @ManyToOne
+    @JoinColumn(name = "user_type_id")
+    private UserType userType;
+
+    @ManyToOne
+    @JoinColumn(name = "subscription_type_id")
+    private SubscriptionType subscriptionType;
+}
