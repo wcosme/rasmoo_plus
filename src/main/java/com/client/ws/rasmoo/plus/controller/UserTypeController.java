e164f965e0ab73e98d750f0d165b32a7ad6b088f
Wallace Gonçalves <wallacecosme@gmail.com>
2024-01-14 18:10:28 -0300

    Controllers created

---
+++ src/main/java/com/client/ws/rasmoo/plus/controller/UserTypeController.java
@@ -0,0 +1,9 @@
+package com.client.ws.rasmoo.plus.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user-type")
+public class UserTypeController {
+}
