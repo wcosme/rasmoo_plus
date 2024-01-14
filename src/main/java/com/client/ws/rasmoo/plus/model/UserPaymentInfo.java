ab8d849645e29c2fb9a08ab1a9de0aef67a02733
Wallace Gonçalves <wallacecosme@gmail.com>
2024-01-14 17:40:19 -0300

    Add entity UserPaymentInfo

---
+++ src/main/java/com/client/ws/rasmoo/plus/model/UserPaymentInfo.java
@@ -0,0 +1,45 @@
+package com.client.ws.rasmoo.plus.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "user_payment_info")
+public class UserPaymentInfo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_payment_info_id")
+    private Long id;
+
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    @Column(name = "card_expiration_month")
+    private Long cardExpirationMonth;
+
+    @Column(name = "card_expiration_year")
+    private Long cardExpirationYear;
+
+    @Column(name = "card_security_code")
+    private String cardSecurityCode;
+
+    private BigDecimal price;
+
+    @Column(name = "dt_payment")
+    private LocalDate dtPayment;
+
+    @OneToOne
+    @JoinColumn(name = "users_id")
+    private User user;
+
+}
